8022ac60e26a394b161108f32876cd22d556f305
Guilherme Souza <gui.eng.santos@gmail.com>
2020-08-19 19:03:27 -0300

    Configurado a caixa onde ficarão as imagens dos jogos

---
+++ Macactus-Web/src/components/GameBox/index.js
@@ -0,0 +1,18 @@
+﻿import React from 'react';
+import './style.css';
+
+function GameBox(){
+    return(
+        <div className="game-container">
+            <img src="https://images.habbo.com/habbo-web/america/pt/assets/images/app_summary_image-1200x628.85a9f5dc.png"
+            className="game-image"/>
+            <div className="box-content">
+                <span className="class">212 a 314</span>
+                <h3>Habbo</h3>
+                <span className="materia" id="subject">Português</span>
+            </div>
+        </div>
+    );
+}
+
+export default GameBox;
