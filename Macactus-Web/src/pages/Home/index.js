fb510b21d171dd406d36b7fc375b4b196aaa603a
Guilherme Souza <gui.eng.santos@gmail.com>
2020-08-23 12:53:50 -0300

    incluido o carrossel

--- Macactus-Web/src/pages/Home/index.js
+++ Macactus-Web/src/pages/Home/index.js
@@ -3,16 +3,35 @@
 import PageHeader from '../../components/PageHeader';
 import Menu from '../../components/Menu';
 import GameBox from '../../components/GameBox';
-
 import './style.css';
+import {Swiper, SwiperSlide} from 'swiper/react';
+import SwiperCore, {EffectFade} from 'swiper';
+import 'swiper/swiper-bundle.css';
 
-
+SwiperCore.use(EffectFade);
 
 function Home(){
+    const Games = [
+
+        (<SwiperSlide key={'1'}>
+            <GameBox />
+        </SwiperSlide>),
+        (<SwiperSlide key={'2'}>
+          <GameBox />
+        </SwiperSlide>),
+        (<SwiperSlide key={'3'}>
+         <GameBox />
+        </SwiperSlide>),
+        (<SwiperSlide key={'4'}>
+            <div />
+       </SwiperSlide>)
+    ];
 return (
     <div className="home">
         <PageHeader />
-        <GameBox />
+        <Swiper id="games-slider" slidesPerView={2} spaceBetween={"33%"} in effectFade>
+            {Games}
+        </Swiper>
         <Menu />
     </div>
 );
